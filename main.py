9c8a7bda6d4914d21b8a8c5fea4b35590aa45095
bob_cabelinho <victor.nicacio@usp.br>
2021-07-26 17:21:40 -0300

    ja esta printando media e desvio padrão

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
 # usr/bin/python
 
+from pyspark.sql.functions import mean, stddev
 from pyspark.sql import SparkSession
-import pyspark
 
 spark = SparkSession.builder.master("local").appName("EP2DSID").config('spark.ui.port', '4050').getOrCreate()
 
-def analise():
+def media(df, info):
+    df.select([mean(info)]).show()
+
+def desvio(df, info):
+    df.select([stddev(info)]).show()
+
+def analise(info):
     df_spark = spark.read.csv('dataset/sample.csv', header=True, inferSchema=True)
-    df_spark.printSchema()
+
+    column = df_spark.select(info)
+    column.show()
+    column.describe().show()
+
+    media(df_spark, info)
+    desvio(df_spark, info)
 
 if __name__ == '__main__':
 
-    while (True):
-        #print('\nEscolha um periodo para analisar [19XX-20XX]')
-        #periodo_input = input('> ')
-        periodo_input = '1929-2000'
-        periodo = periodo_input.split('-')
+    print('\nEscolha um periodo para analisar [19XX-20XX]')
+    #periodo_input = input('> ')
+    periodo_input = '1929-2000'
+    periodo = periodo_input.split('-')
 
 
-        print('\nEscolha o tipo de informação [temperatura, velocidade,...]')
-        info_input = input(f'({periodo_input}) > ')
+    print('\nEscolha o tipo de informação [temperatura, velocidade,...]')
+    info_input = input(f'({periodo_input}) > ')
 
-        analise()
+    analise(info_input)
